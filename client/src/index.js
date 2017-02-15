0588afe787b449728d05196da1790c192054e331
maniksingh92 <maniksingh92@gmail.com>
2017-02-15 16:18:06 +0530

    undo remove bootstrap

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 ReactDOM.render(
