e132d9fdc4e4cb7fbdfb17798189d81f82a436ee
maniksingh92 <maniksingh92@gmail.com>
2017-02-17 05:33:32 +0530

    update fetchival api definition

--- client/src/services/api/index.js
+++ client/src/services/api/index.js
@@ -2,6 +2,6 @@ import apiConfig from './config';
 import fetchival from 'fetchival';
 
 export const fetchApi = (endPoint, payload = {}, method = 'get', headers = {}) => {
-  return fetchival(`${apiConfig.url}${endPoint}`)[method.toLowerCase()](payload)
+  return fetchival(`${apiConfig.url}${endPoint}`, headers)[method.toLowerCase()](payload)
   .catch((e) => {throw e});
 }
